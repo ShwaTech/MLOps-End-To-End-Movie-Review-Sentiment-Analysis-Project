798af8cb995c7b500f1c30833db57eb5b8e32e68
ShwaTech <shwakesh13579@gmail.com>
2025-10-16 21:27:41 +0300

    Initial Project Structure Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-To-End MLOps Movie-review-Sentiment-Analysis',
+    author='Mohamed Fathy',
+    license='',
+)
